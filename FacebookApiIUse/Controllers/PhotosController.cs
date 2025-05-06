52872e9f3fdf193f1d2d144a5a5066bcbc2199d3
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-05-06 17:43:10 +0300

    Add project files.

---
+++ FacebookApiIUse/Controllers/PhotosController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FacebookApiIUse.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+    }
+}
